c3dfc5fd8206cd235c3f927b1710c0e9fbf3b1e6
kimminkyu <kmk50009@naver.com>
2021-07-15 15:54:12 +0900

    Example 레이아웃, 파이어베이스 설정, 회원가입

--- app/src/main/java/study/example/MainActivity.kt
+++ app/src/main/java/study/example/MainActivity.kt
@@ -2,10 +2,33 @@ package study.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        val joinBtnClicked = findViewById<Button>(R.id.joinBtn)
+        joinBtnClicked.setOnClickListener {
+            auth.createUserWithEmailAndPassword("abc@abc.com", "12341234")
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this, "no", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+        }
     }
 }
